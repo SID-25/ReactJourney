41b53803d2cfd633a0015f16c3043c63be843ac9
Sid <work.sid25@gmail.com>
2024-04-05 20:36:37 +0530

    comments in the TextForm.js for better understanding of the Events used

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 
 export default function TextArea(props) {
+  // State for storing the text value of the textarea
   const [text, setText] = useState('Enter the text here')
 
+  // Function to convert the text to uppercase and update the state
   const handleUpclick = () => {
-   let newText = text.toUpperCase();
+    let newText = text.toUpperCase();
     setText(newText);
   }
 
+  // Function to handle changes in the textarea content and update the state
   const handleOnChange = (event) => {
     setText(event.target.value) 
   }
 
+  // Function to clear the textarea content when it receives focus
   const handleFocus = () => {
     setText(''); // Clear the textarea when it receives focus
   }
 
   return (
     <div className="mb-3">
+      {/* Label for the textarea */}
       <label htmlFor="mybox" className="form-label">{props.heading}</label>
+      {/* Textarea element */}
       <textarea
         className="form-control"
-        value={text}
-        onChange={handleOnChange}
-        onFocus={handleFocus} // Clear the textarea when it receives focus
+        value={text} // Value of the textarea set to the state 'text'
+        onChange={handleOnChange} // Event handler for changes in textarea content
+        onFocus={handleFocus} // Event handler for clearing textarea content when it receives focus
         id="mybox"
         rows="8"
       ></textarea>
+      {/* Button to convert textarea content to uppercase */}
       <button className='btn btn-primary' onClick={handleUpclick}>Convert to Uppercase</button>
     </div>
   );
